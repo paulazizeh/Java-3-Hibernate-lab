b9d67b8fae633b4cb7f93a397d444b498e775793
paulazizeh <70772926+paulazizeh@users.noreply.github.com>
2021-01-26 12:56:46 -0500

    Initial commit

---
+++ src/edu/cscc/Main.java
@@ -0,0 +1,10 @@
+package edu.cscc;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("LacklusterVideo");
+    }
+}
